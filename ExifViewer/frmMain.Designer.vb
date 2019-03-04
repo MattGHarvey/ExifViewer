a753bf72353990a04cb074afb9f068be0da56ceb
Matt Harvey <matt@75central.com>
2019-03-04 08:22:37 -0600

    Initial Commit

---
+++ ExifViewer/frmMain.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ofdSelect = New System.Windows.Forms.OpenFileDialog()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(23, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(493, 391)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.UseWaitCursor = True
+        '
+        'ofdSelect
+        '
+        Me.ofdSelect.Filter = "JPG files|*.jpg|JPEG files|*.jpeg"
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Location = New System.Drawing.Point(532, 105)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(231, 90)
+        Me.btnSelect.TabIndex = 1
+        Me.btnSelect.Text = "Select Photo"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        Me.btnSelect.UseWaitCursor = True
+        '
+        'frmMain
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "frmMain"
+        Me.Text = "EXIFVIewer"
+        Me.UseWaitCursor = True
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ofdSelect As OpenFileDialog
+    Friend WithEvents btnSelect As Button
+End Class
---
+++ ExifViewer/frmMain.vb
@@ -0,0 +1,147 @@
+﻿Imports System
+Imports System.IO
+Imports System.Math
+Imports MetadataExtractor
+Imports MetadataExtractor.Formats.Exif
+Public Class frmMain
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox1_DragDrop(sender As Object, e As DragEventArgs) Handles PictureBox1.DragDrop
+        MsgBox("test", vbOK)
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+
+    End Sub
+
+    Private Sub frmMain_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        'Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+        'For Each path In files
+        'MsgBox(path)
+        ' Next
+    End Sub
+
+    Private Sub frmMain_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim files As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+            For Each file In files
+                MessageBox.Show(file)
+            Next
+        End If
+    End Sub
+
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        Me.ofdSelect.ShowDialog()
+        If Me.ofdSelect.FileName.Trim <> "" Then
+            DisplayEXIF(Me.ofdSelect.FileName.Trim)
+        End If
+
+    End Sub
+    Private Sub DisplayEXIF(filename As String)
+        Dim directories As IEnumerable(Of MetadataExtractor.Directory) = ImageMetadataReader.ReadMetadata(filename)
+        Dim directory = directories.OfType(Of ExifSubIfdDirectory)().FirstOrDefault()
+        Dim deviceDirectory = directories.OfType(Of ExifIfd0Directory)().FirstOrDefault
+
+        Dim gpsdirectory = directories.OfType(Of GpsDirectory)().FirstOrDefault()
+        Dim latitude = Nothing
+        Dim datetime = Nothing
+        Dim Longitude = Nothing
+        Dim aperture As String = Nothing
+        Dim ColorSpace = Nothing
+        Dim TagWhiteBalance
+        Dim camera As String = ""
+        Dim focallength As String = ""
+        Dim ISO As String = ""
+        Dim ShutterSpeed As String = ""
+        Dim TagExposureBias
+        Dim TagMeteringMode
+        Dim TagExifImageWidth
+        Dim TagFlash
+        Dim TagExifImageHeight
+        Dim TagLensSpecification
+        Dim TagLensMake
+        Dim TagLens
+        Dim TagLensModel
+        Dim TagExposureIndex
+        If directory.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, datetime) Then
+            Console.WriteLine("datetime:" & datetime)
+        End If
+
+        If directory IsNot Nothing Then
+            aperture = directory.GetDescription(ExifDirectoryBase.TagAperture)
+
+            focallength = directory.GetDescription(ExifDirectoryBase.TagFocalLength)
+            ColorSpace = directory.GetDescription(ExifDirectoryBase.TagColorSpace)
+            ISO = directory.GetDescription(ExifDirectoryBase.TagIsoEquivalent)
+            ShutterSpeed = directory.GetDescription(ExifDirectoryBase.TagShutterSpeed)
+            TagWhiteBalance = directory.GetDescription(ExifDirectoryBase.TagWhiteBalance)
+            TagExposureBias = directory.GetDescription(ExifDirectoryBase.TagExposureBias)
+            TagMeteringMode = directory.GetDescription(ExifDirectoryBase.TagMeteringMode)
+            TagFlash = directory.GetDescription(ExifDirectoryBase.TagFlash)
+            TagExifImageWidth = directory.GetDescription(ExifDirectoryBase.TagExifImageWidth)
+            TagExifImageHeight = directory.GetDescription(ExifDirectoryBase.TagExifImageHeight)
+            TagLensSpecification = directory.GetDescription(ExifDirectoryBase.TagLensSpecification)
+            TagLensMake = directory.GetDescription(ExifDirectoryBase.TagLensMake)
+            TagLens = directory.GetDescription(ExifDirectoryBase.TagLens)
+            TagLensModel = directory.GetDescription(ExifDirectoryBase.TagLensModel)
+            TagExposureIndex = directory.GetDescription(ExifDirectoryBase.TagExposureIndex)
+
+            If ShutterSpeed IsNot Nothing Then
+                ShutterSpeed = FixSS(ShutterSpeed.Replace("sec", "")) & "sec"
+            End If
+            Console.WriteLine("focallength:" & focallength)
+            Console.WriteLine("aperture:" & aperture)
+            Console.WriteLine("ColorSpace:" & ColorSpace)
+            Console.WriteLine("ShutterSpeed:" & ShutterSpeed)
+            Console.WriteLine("ISO:" & ISO)
+            Console.WriteLine("ShutterSpeed:" & ShutterSpeed)
+
+            Console.WriteLine("TagExposureBias:" & TagExposureBias)
+            Console.WriteLine("TagMeteringMode:" & TagMeteringMode)
+
+            Console.WriteLine("TagFlash:" & TagFlash)
+
+            Console.WriteLine("TagLensModel:" & TagLensModel)
+            Console.WriteLine("TagExposureIndex:" & TagExposureIndex)
+
+        End If
+        If deviceDirectory IsNot Nothing Then
+            camera = deviceDirectory.GetDescription(ExifDirectoryBase.TagModel.ToString)
+            If camera IsNot Nothing Then
+                camera = camera.Replace(",", "")
+            End If
+            Console.WriteLine("camera:" & camera)
+        End If
+        If gpsdirectory IsNot Nothing Then
+            gpsdirectory.GetGeoLocation()
+            latitude = gpsdirectory.GetGeoLocation.Latitude
+            Longitude = gpsdirectory.GetGeoLocation.Longitude
+
+            Console.WriteLine("Coordinates:" & latitude & "," & Longitude)
+            'filename = Path.GetFileName(filename)
+
+
+
+        End If
+    End Sub
+    Public Function FixSS(ss As String)
+        Dim sspeed() As String
+        Dim denom As Int16
+
+        If ss.Contains("/") Then
+            sspeed = ss.Split("/")
+            If IsNumeric(sspeed(1)) Then
+                denom = Convert.ToInt16(sspeed(1))
+
+                denom = Round(denom / 5) * 5
+                Return "1/" & denom.ToString()
+            End If
+        End If
+        Return ss
+    End Function
+End Class
